e915a797abc50ce22ff00e9f253dcfb584921f8c
Ali Mohamed <ali405930@fci.bu.edu.eg>
2026-02-26 11:44:52 +0200

    feat(core): display & pixel buffer

--- core/display.go
+++ core/display.go
@@ -9,3 +9,18 @@ type Display struct {
 	Pixels [Width * Height]bool
 	Dirty  bool
 }
+
+func (d *Display) Clear() {
+	d.Pixels = [Width * Height]bool{}
+	d.Dirty = true
+}
+
+func (d *Display) TogglePixel(x, y byte) bool {
+	wx := uint16(x) % Height
+	wy := uint16(y) % Width
+	idx := wy*Width + wx
+	erased := d.Pixels[idx]
+	d.Pixels[idx] = !d.Pixels[idx]
+	d.Dirty = true
+	return erased
+}
