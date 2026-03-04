8ac672caae317c649aa6f21709c68d88f8f87ee4
Ali Mohamed <ali405930@fci.bu.edu.eg>
2026-03-04 06:05:58 +0200

    main: v1.0 executable

--- main.go
+++ main.go
@@ -2,70 +2,37 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
+	"os"
 
 	"github.com/9lynd/Goch8/core"
+	ebitenrenderer "github.com/9lynd/Goch8/renderer/ebiten"
 )
-
-const (
-	Width  = 64
-	Height = 32
-)
-
-func render(d *core.Display) {
-	for y := 0; y < Height; y++ {
-		for x := 0; x < Width; x++ {
-			if d.Pixels[y*Width+x] {
-				fmt.Print("█")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
-	}
-}
-
-func anyPixel(d *core.Display) bool {
-	for _, p := range d.Pixels {
-		if p {
-			return true
-		}
-	}
-	return false
-}
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: chip8 <rom path>")
+		os.Exit(1)
+	}
+
+	romPath := os.Args[1]
+	romData, err := os.ReadFile(romPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read ROM: %v\n", err)
+		os.Exit(1)
+	}
+
 	display := &core.Display{}
 	keyboard := &core.Keyboard{}
 	cpu := core.NewCPU(display, keyboard)
 
-	romData, err := ioutil.ReadFile("roms/IBM_Logo.ch8")
-	if err != nil {
-		log.Fatalf("Failed to load ROM: %v", err)
+	if err := cpu.LoadROM(romData); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load ROM: %v\n", err)
+		os.Exit(1)
 	}
 
-	err = cpu.LoadROM(romData)
-	if err != nil {
-		log.Fatalf("Error loading ROM: %v", err)
-	}
-
-	const maxCycles = 5000
-	for i := 0; i < maxCycles; i++ {
-		if err := cpu.Cycle(); err != nil {
-			log.Printf("Stopped: %v", err)
-			break
-		}
-		if cpu.Display.Dirty {
-			if anyPixel(cpu.Display) {
-				render(cpu.Display)
-			}
-			cpu.Display.Dirty = false
-		}
-	}
-
-	fmt.Println("ROM bytes at 0x200:")
-	for i := 0; i < 16; i++ {
-		fmt.Printf("0x%03X: 0x%02X\n", 0x200+i, cpu.Memory[0x200+i])
+	renderer := &ebitenrenderer.Renderer{}
+	if err := renderer.Run(cpu); err != nil {
+		fmt.Fprintf(os.Stderr, "renderer error: %v\n", err)
+		os.Exit(1)
 	}
 }
